74d1625df7471abeeba290fe8584e9b7f50b9165
Gloria <gloriacheung812@gmail.com>
2022-03-25 00:03:41 +0000

    flatten nested arrays

---
+++ flatten.js
@@ -0,0 +1,36 @@
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log("the arrays are equal");
+  } else {
+    console.log("the arrays are not equal");
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  let result = true;
+  for (let i = 0; i < arr1.length; i ++) {
+    if (arr1[i] === arr2[i]) {
+      result = true;
+    } else {
+      result = false;
+    }
+  }
+  return result;
+};
+
+const flatten = function(arr) {
+  let result = [];
+  for (let i = 0; i < arr.length; i ++) {
+    if (!Array.isArray(arr[i])) {
+      result.push(arr[i]);
+    } else {
+      result.push(...arr[i]);
+    }
+  }
+  return result;
+};
+
+//test
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
+assertArraysEqual(flatten([1, [1, 2], [2, 3], 4, 4]), [1, 1, 2, 2, 3, 4, 4]);
+assertArraysEqual(flatten([0, [1, 1], [9, 9, 9]]), [0, 1, 1, 9, 9, 9]);
