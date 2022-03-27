1bb68988dc32f523c29d73eca7a9bb37e9e37148
Gloria <gloriacheung812@gmail.com>
2022-03-27 15:38:43 +0000

    checking letter position in sentence and returning array of positions per letter

---
+++ letterPositions.js
@@ -0,0 +1,42 @@
+const eqArrays = function(arr1, arr2) {
+  let result = true;
+  for (let i = 0; i < arr1.length; i ++) {
+    if (arr1[i] === arr2[i]) {
+      result = true;
+    } else {
+      result = false;
+      break;
+    }
+  }
+  return result;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log("the arrays are equal");
+  } else {
+    console.log("the arrays are not equal");
+  }
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i ++) {
+    if (sentence[i] !== " ") {
+      if (!results[sentence[i]]) {
+        results[sentence[i]] = [i];
+      } else {
+        results[sentence[i]].push(i);
+      }
+    }
+  }
+  return results;
+};
+
+
+assertArraysEqual(letterPositions("hello")["l"], [2, 3]);
+assertArraysEqual(letterPositions("i am a student")["a"], [2, 5]);
+assertArraysEqual(letterPositions("i like to code")["i"], [0, 3]);
+assertArraysEqual(letterPositions("food is awesome")["o"], [1, 2, 12]);
+assertArraysEqual(letterPositions("my name is gloria")["i"], [8, 15]);
+assertArraysEqual(letterPositions("lighthouse in the house")["i"], [1, 11]);
