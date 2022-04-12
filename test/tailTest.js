35a6a425049db368fc0fd8dd6a2e1286a11aea56
Gloria <gloriacheung812@gmail.com>
2022-04-12 12:21:17 +0000

    added mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,15 +1,37 @@
 const tail = require("../tail");
-const assertEqual = require("../assertEqual");
+const assert = require('chai').assert;
 
-const words = ["hello", "my", "name", "is", "Gloria"];
-tail(words);
-assertEqual(tail(words).length, words.length - 1);
-assertEqual(tail(words)[0], "my");
-assertEqual(tail(words)[1], "name");
-assertEqual(words.length, 5);
+describe("#tail", () => {
+  it("returns ['my', 'name', 'is', 'Gloria'] for ['hello', 'my', 'name', 'is', 'Gloria']", () => {
+    const words = ['hello', 'my', 'name', 'is', 'Gloria'];
+    assert.deepEqual(tail(words), ['my', 'name', 'is', 'Gloria']);
+  });
 
-const word = ["single"];
-assertEqual(tail(word).length, 0);
+  it("returns 4 for length of tail(['hello', 'my', 'name', 'is', 'Gloria'])", () => {
+    const words = ['hello', 'my', 'name', 'is', 'Gloria'];
+    assert.deepEqual(tail(words).length, words.length - 1);
+  });
 
-const empty = [];
-assertEqual(tail(empty).length,0);
+  it("returns 'my' for first element of tail(['hello', 'my', 'name', 'is', 'Gloria'])", () => {
+    const words = ['hello', 'my', 'name', 'is', 'Gloria'];
+    assert.deepEqual(tail(words)[0], 'my');
+  });
+
+  it("returns 'name' for second element of tail(['hello', 'my', 'name', 'is', 'Gloria'])", () => {
+    const words = ['hello', 'my', 'name', 'is', 'Gloria'];
+    assert.deepEqual(tail(words)[1], 'name');
+  });
+
+  it("returns 5 for length of array ['hello', 'my', 'name', 'is', 'Gloria']", () => {
+    const words = ['hello', 'my', 'name', 'is', 'Gloria'];
+    assert.deepEqual(words.length, 5);
+  });
+
+  it("returns 0 for length of array tail(['single'])", () => {
+    assert.deepEqual(tail(['single']).length, 0);
+  });
+
+  it("returns 0 for length of array tail([])", () => {
+    assert.deepEqual(tail([]).length, 0);
+  });
+});
