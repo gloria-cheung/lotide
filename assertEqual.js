ae5887f8bf417ce0cc9845d5b36af93f0e3be081
Gloria <gloriacheung812@gmail.com>
2022-04-12 11:28:11 +0000

    dry up code using module.export and require

--- assertEqual.js
+++ assertEqual.js
@@ -6,7 +6,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(2, 3);
-assertEqual(1, 1);
-assertEqual("hello", "hello");
+module.exports = assertEqual;
---
+++ test/assertEqualTest.js
@@ -0,0 +1,6 @@
+const assertEqual = require("../assertEqual");
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(2, 3);
+assertEqual(1, 1);
+assertEqual("hello", "hello");
